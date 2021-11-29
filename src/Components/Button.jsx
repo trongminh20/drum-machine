10ea1d83ed4af061e21ebdca5b4997ac7d5e6d59
trongminh20 <trongminh20@gmail.com>
2021-11-28 16:22:23 -0800

    styling

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import "../SCSS/btn.scss";
+import { mouseLeave, mouseOver } from "../Events";
 export default function Button({ _value, _handleClick, _id, _sound }) {
     return (
-        <>
+        <div className="drum-pad btn"
+            onMouseOver={mouseOver}
+            onMouseLeave={mouseLeave}
+            onClick={_handleClick} >
             <audio id={_id}>
                 <source src={_sound} type="audio/mp3" />
             </audio>
-            <button onClick={_handleClick} className="btn drum-pad">{_value}</button>
-        </>
+            {_value}
+        </div>
     )
 }
---
+++ src/Events.js
@@ -0,0 +1,6 @@
+export const mouseOver = e => {
+    e.target.style.background = "#8C693E";
+}
+export const mouseLeave = e => {
+    e.target.style.background = "#a0d9d9";
+}
